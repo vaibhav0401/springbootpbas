98ff0546394b9bd77b8f0b0a86e500cdf9840cf9
vaibhav.gupta <vaibhav.gupta@avizva.com>
2023-06-04 00:06:17 +0530

    inital commit

---
+++ src/main/java/com/example/pbas/PBAS/PbasApplication.java
@@ -0,0 +1,13 @@
+package com.example.pbas.PBAS;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PbasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PbasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/pbas/PBAS/PbasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.pbas.PBAS;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PbasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
