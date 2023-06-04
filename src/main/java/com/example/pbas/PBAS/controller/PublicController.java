8f41295720dfcf329cef1ed6c07e158819ba473b
vaibhav.gupta <vaibhav.gupta@avizva.com>
2023-06-04 13:55:26 +0530

    adding basic auth

--- src/main/java/com/example/pbas/PBAS/controller/PublicController.java
+++ src/main/java/com/example/pbas/PBAS/controller/PublicController.java
@@ -5,6 +5,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/public")
 public class PublicController {
 
 	@RequestMapping( value = "/hello" ,method = RequestMethod.POST)
--- src/main/java/com/example/pbas/PBAS/security/SecurityConfig.java
+++ src/main/java/com/example/pbas/PBAS/security/SecurityConfig.java
@@ -2,11 +2,23 @@ package com.example.pbas.PBAS.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
-public class SecurityConfig {
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http.csrf().disable()
+			.authorizeRequests()
+			.antMatchers("/public/**").permitAll()// Allow access to all contacts for everyone
+			.anyRequest().authenticated()
+			.and()
+			.httpBasic();
+	}
 	@Bean
 	public PasswordEncoder passwordEncoder() {
 		return new BCryptPasswordEncoder();
